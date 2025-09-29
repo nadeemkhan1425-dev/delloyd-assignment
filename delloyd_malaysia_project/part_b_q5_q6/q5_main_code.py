9d7b4272990d894954f3bf3e87be45402edfbe33
Nadeem Khan <nadeemkhan1425@gmail.com>
2025-09-29 17:48:26 +0800

    Delete delloyd_malaysia_project directory

--- delloyd_malaysia_project/part_b_q5_q6/q5_main_code.py
+++
@@ -1,8 +0,0 @@
-from q5_similarity.similarity import string_similarity
-
-s1 = input("Enter first string: ")
-s2 = input("Enter second string: ")
-
-similarity, report = string_similarity(s1, s2)
-print(f"Similarity: {similarity:.2f}%")
-print(report)
--- delloyd_malaysia_project/part_b_q5_q6/q5_similarity/__init__.py
+++
@@ -1,5 +0,0 @@
-# q5_similarity/__init__.py
-
-from .similarity import string_similarity
-
-__all__ = ["string_similarity"]
